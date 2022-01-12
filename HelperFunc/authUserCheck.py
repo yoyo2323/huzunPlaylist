8cf827485ce2f70c2fdc1ef25cf120765816b466
ha <84624971+HuzunluArtemis@users.noreply.github.com>
2022-01-12 19:03:05 +0300

    3

--- HelperFunc/authUserCheck.py
+++ HelperFunc/authUserCheck.py
@@ -4,21 +4,15 @@ from pyrogram.types.messages_and_media.message import Message
 from config import Config
 
 async def AuthUserCheck(message: Message):
-    if 0 in Config.AUTH_IDS:
-        return True
-    elif message.from_user.id in Config.AUTH_IDS:
-        return True
-    elif message.chat.id in Config.AUTH_IDS:
-        return True
-    else:
-        return False
+    if 0 in Config.AUTH_IDS: return True
+    elif message.from_user.id in Config.AUTH_IDS: return True
+    elif message.from_user.id in Config.PREMIUM_USERS: return True
+    elif message.chat.id in Config.AUTH_IDS: return True
+    else: return False
 
 def AuthUserCheckSync(message: Message):
-    if 0 in Config.AUTH_IDS:
-        return True
-    elif message.from_user.id in Config.AUTH_IDS:
-        return True
-    elif message.chat.id in Config.AUTH_IDS:
-        return True
-    else:
-        return False
+    if 0 in Config.AUTH_IDS: return True
+    elif message.from_user.id in Config.AUTH_IDS: return True
+    elif message.from_user.id in Config.PREMIUM_USERS: return True
+    elif message.chat.id in Config.AUTH_IDS: return True
+    else: return False
